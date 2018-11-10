8aea5981e9bd62750fc3efa51c37a61e4e961ae1
Vitor Freitas <vitorfs@gmail.com>
2018-11-10 17:06:31 +0200

    Update static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
