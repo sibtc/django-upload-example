95efeca95ba64754638eeb0350524d8acc4eb62c
Sivaram Kannan <skannan@mesosphere.com>
2019-08-02 17:51:20 +0530

    Add login related logic.

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from mysite.core import views
 
@@ -17,6 +18,10 @@ urlpatterns = [
     path('class/books/upload/', views.UploadBookView.as_view(), name='class_upload_book'),
 
     path('admin/', admin.site.urls),
+
+    #Authentication urls
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 if settings.DEBUG:
